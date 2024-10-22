c498de8045c37fdc60cad9d30b40430ddc46f97b
fvon-der <fvon-der@student.42heilbronn.de>
2024-10-22 11:16:43 +0200

    DONE : Print specifiers char, str
    TODO: Testing
    
    TODO : rest of specifiers

---
+++ src/ft_print_char.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_char.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 08:57:12 by fvon-de           #+#    #+#             */
+/*   Updated: 2024/10/22 08:58:22 by fvon-de          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+int	ft_print_c(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_print_char(char c, t_flags flags)
+{
+	int	count;
+
+	count = 0;
+	if (flags.left == 1)
+		count += ft_print_c(c);
+	count += ft_pad_width(flags.width, 1, flags.zero);
+	if (flags.left == 0)
+		count += ft_print_c(c);
+	return (count);
+}
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:48:08 by fvon-de           #+#    #+#             */
-/*   Updated: 2024/10/22 10:58:21 by fvon-de          ###   ########.fr       */
+/*   Updated: 2024/10/22 11:15:17 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,11 +78,24 @@ static int	ft_parse(char *str, va_list args)
 		{
 			i = ft_parse_flags(str, i, args, &flags);
 			if (str[i] != '\0' && flags.spec > 0 && ft_istype(str[i]))
-				// DO THE STUFF 
-                ;
+				count += ft_print_arg(str[i], args, flags);
 		}
 		else
 			count += ft_print_c(str[i]);
 	}
+	return (count);
+}
+
+int	ft_print_arg(char type, va_list args, t_flags flags)
+{
+	int	count;
+
+	count = 0;
+	if (type == '%')
+		count += ft_print_char('%', flags);
+	else if (type == 'c')
+		count += ft_print_char(va_arg(args, int), flags);
+	else if (type == 's')
+		count += ft_print_str(va_arg(args, const char *), flags);
 	return (count);
 }
