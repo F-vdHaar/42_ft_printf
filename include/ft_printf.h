393ac34e724b8a8fcbd07487ac7b36a29eac0a70
fvon-der <fvon-der@student.42heilbronn.de>
2024-10-22 11:00:15 +0200

    WP : Flag logic
    func flag parser, flag struct
    
    TODO : Testing

---
+++ include/ft_printf.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 10:57:20 by fvon-de           #+#    #+#             */
+/*   Updated: 2024/10/22 10:58:10 by fvon-de          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "../../libft/include/libft.h" 
+
+/* ---------- FLAGS --------------------- */
+typedef struct s_flags
+{
+	int	spec;
+	int	width;
+	int	left;
+	int	zero;
+	int	star;
+	int	precision;
+	int	hash;
+	int	space;
+	int	plus;
+}		t_flags;
+
+t_flags	ft_flags_init(void);
+t_flags	ft_flag_left(t_flags flags);
+t_flags	ft_flag_digit(char c, t_flags flags);
+t_flags	ft_flag_width(va_list args, t_flags flags);
+int		ft_flag_precision(const char *str, int pos,
+			va_list args, t_flags *flags);
+
+/* ---------- PRINTF -------------------- */
+int		ft_printf(const char *format, ...);
+int		ft_print_arg(char type, va_list args, t_flags flags);
+
+#endif
---
+++ src/ft_printf.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 10:48:08 by fvon-de           #+#    #+#             */
+/*   Updated: 2024/10/22 10:58:21 by fvon-de          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+static int	ft_parse_flags(const char *str, int i, va_list args, t_flags *flags);
+static int	ft_parse(char *str, va_list args);
+
+int	ft_printf(const char *format, ...)
+{
+	va_list		args;
+	int			count;
+	char		*str;
+
+	if (!format || *format == '\0')
+		return (0);
+	str = ft_strdup(format);
+	if (!str || *str == '\0')
+		return (0);
+	count = 0;
+	va_start(args, format);
+	count = ft_parse(str, args);
+	va_end(args);
+	free(str);
+	return (count);
+}
+
+static int	ft_parse_flags(const char *str, int i, va_list args, t_flags *flags)
+{
+	while (str[++i] && ft_isflag(str[i]))
+	{
+		if (str[i] == '#')
+			flags->hash = 1;
+		if (str[i] == ' ')
+			flags->space = 1;
+		if (str[i] == '+')
+			flags->plus = 1;
+		if (str[i] == '0' && flags->left == 0 && flags->width == 0)
+			flags->zero = 1;
+		if (str[i] == '-')
+			*flags = ft_flag_left(*flags);
+		if (str[i] == '.')
+			i = ft_flag_precision(str, i, args, flags);
+		if (str[i] == '*')
+			*flags = ft_flag_width(args, *flags);
+		if (ft_isdigit(str[i]))
+			*flags = ft_flag_digit(str[i], *flags);
+		if (ft_istype(str[i]))
+		{
+			flags->spec = str[i];
+			break ;
+		}
+	}
+	return (i);
+}
+
+static int	ft_parse(char *str, va_list args)
+{
+	int		i;
+	int		count;
+	t_flags	flags;
+
+	i = -1;
+	count = 0;
+	while (str[++i])
+	{
+		flags = ft_flags_init();
+		if (str[i] == '%' && str[i + 1] != '\0')
+		{
+			i = ft_parse_flags(str, i, args, &flags);
+			if (str[i] != '\0' && flags.spec > 0 && ft_istype(str[i]))
+				// DO THE STUFF 
+                ;
+		}
+		else
+			count += ft_print_c(str[i]);
+	}
+	return (count);
+}
