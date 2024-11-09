b1a28638af15049cea5fd022364516bba0fb6f88
fvon-der <fvon-der@student.42heilbronn.de>
2024-11-09 13:18:31 +0100

    Cleanup : SQUASHME

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:57:20 by fvon-de           #+#    #+#             */
-/*   Updated: 2024/10/24 15:45:47 by fvon-de          ###   ########.fr       */
+/*   Updated: 2024/11/09 13:02:22 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include "../lib/libft/include/libft.h" 
+# include "libft.h" 
 
 /* ---------- FLAGS --------------------- */
 typedef struct s_flags
